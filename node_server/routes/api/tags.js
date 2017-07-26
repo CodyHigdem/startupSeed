56cf609573ed28513a509bc4fd0fcf473abe568f
Cody <meanderingmonarch@gmail.com>
2017-07-26 16:23:36 -0500

    created tags

--- node_server/routes/api/tags.js
+++ node_server/routes/api/tags.js
@@ -2,7 +2,11 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Article = mongoose.model('Article');
 
-
+router.get('/', function(req, res, next){
+	Article.find().disctinct('tagList').then(function(tags){
+		return res.json({ tags: tags });
+	}).catch(next);
+});
 
 module.exports = router;
 
