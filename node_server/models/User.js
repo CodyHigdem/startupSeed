c0072620e2ee71b596b7f587586291fb95f35f67
Cody <meanderingmonarch@gmail.com>
2017-07-25 20:20:26 -0500

    working on favoriting

--- node_server/models/User.js
+++ node_server/models/User.js
@@ -9,6 +9,7 @@ var UserSchema = new mongoose.Schema({
 	email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
 	bio: String,
 	image: String,
+	favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article'}],
 	hash: String,
 	salt: String
 }, { timestamps: true });
@@ -70,5 +71,30 @@ UserSchema.methods.toProfileJSONFor = function(user){
 		following: false
 	};
 };
+
+//method for user to favorite an article
+
+UserSchema.methods.favorite = function(id){
+	if(this.favorites.indexOf(id) === -1 ){
+		this.favorites.push(id);
+	}
+
+	return this.save();
+};
+
+//method for user to unfavorite an article
+
+UserSchema.methods.unfavorite = function(id){
+	this.favoites.remove( id );
+	return this.save();
+};
+
+//method for frontend to show or not show a favorite
+UserSchema.methods.isFavorite = function(id){
+	return this.favorites.some(function(favoriteId){
+		return favoriteId.toString() === id.toString();
+	});
+};
+
 
 mongoose.model('User', UserSchema);
